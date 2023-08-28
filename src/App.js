252ca57a73d1049195be499ae03a033eb5ba7219
3mpty <aurelien.corre@outlook.fr>
2023-08-28 20:05:22 +0200

    Credit to me lol

--- src/App.js
+++ src/App.js
@@ -3,14 +3,30 @@ import TodoList from "./components/TodoList";
 
 const App = () => {
       return (
-            <main>
-                  <div className="header">
-                        <h1>
-                              Next <span>!</span>
-                        </h1>
-                  </div>
-                  <TodoList />
-            </main>
+            <>
+                  <main>
+                        <div className="header">
+                              <h1>
+                                    Next <span>!</span>
+                              </h1>
+                        </div>
+                        <TodoList />
+                        <p
+                              style={{
+                                    color: "var(--white)",
+                                    fontFamily: "comfortaa",
+                                    bottom: 5,
+                              }}>
+                              Developped by{" "}
+                              <span
+                                    style={{
+                                          color: "var(--darkBlue)",
+                                    }}>
+                                    3mpty
+                              </span>
+                        </p>
+                  </main>
+            </>
       );
 };
 
